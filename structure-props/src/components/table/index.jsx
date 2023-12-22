8bbdcc5ce90df5f21d163e95d25f3c3d14ea347e
AliyevaSalala <aselale58@gmail.com>
2023-12-22 00:20:57 -0800

    react

---
+++ structure-props/src/components/table/index.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+
+const Table = ({ users }) => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Id</th>
+          <th>Name,UserName</th>
+          <th>Email</th>
+          <th>City</th>
+          <th>phone</th>
+          <th>Company Name</th>
+        </tr>
+      </thead>
+      <tbody>
+        {users.map((user) => {
+          return (
+            <tr>
+              <td>{user.id}</td>
+              <td>
+                {user.name} {user.surname}
+              </td>
+              <td>{user.email}</td>
+              <td>{user.address.city}</td>
+              <td>{user.phone}</td>
+              <td>{user.company.name}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+
+
+export default Table;
---
+++ structure-props/src/layouts/header/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./index.scss";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="container">
+        <div className="header">
+          <h1>Header</h1>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
